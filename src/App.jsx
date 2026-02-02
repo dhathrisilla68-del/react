7ed52c519f4a7a29fe44c832aab1451ff79dfbad
dhathrisilla68-del <dhathrisilla68@gmail.com>
2026-02-02 09:49:00 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+ "./App.css";
+
+export default function App() {
+  const student = {
+    name: "dhathri",
+    regNo: "24CS1001",
+    dept: "ECE",
+    year: "I Year",
+    email: "dhathrisilla@gmail.com",
+    cgpa: "9.4",
+  };
+
+  return (
+    <div className="page">
+      <h1 className="title">Student Information (External CSS)</h1>
+
+      <div className="card">
+        <div className="badge">ACTIVE</div>
+
+        <h2 className="name">{student.name}</h2>
+        <p className="sub">Reg No: <b>{student.regNo}</b></p>
+
+        <div className="grid">
+          <div className="box">
+            <p className="label">Department</p>
+            <p className="value">{student.dept}</p>
+          </div>
+          <div className="box">
+            <p className="label">Year</p>
+            <p className="value">{student.year}</p>
+          </div>
+          <div className="box">
+            <p className="label">Email</p>
+            <p className="value">{student.email}</p>
+          </div>
+          <div className="box">
+            <p className="label">CGPA</p>
+            <p className="value">{student.cgpa}</p>
+          </div>
+        </div>
+
+        <button className="btn">Send Message</button>
+      </div>
+    </div>
+  );
+}
